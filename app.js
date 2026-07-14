fa00559e96be9de67b4c9e753359d3fd0fb4fc55
lzarei1366-creator <l.zarei1366@gmail.com>
2026-07-14 09:11:26 +0330

    Create app.js

---
+++ app.js
@@ -0,0 +1,181 @@
+// ================================
+// اتصال به Supabase
+// ================================
+
+const SUPABASE_URL = "https://ezrvqpnxswpzaygbjeio.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_Jr0xXzuVDA9QzEFize_dvA_ueYO-246";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+
+// ================================
+// ورود کاربران
+// ================================
+
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", loginUser);
+
+async function loginUser() {
+
+    const username =
+    document.getElementById("username").value.trim();
+
+    const password =
+    document.getElementById("password").value.trim();
+
+    if(username==="" || password===""){
+
+        document.getElementById("loginResult").innerHTML=
+        "نام کاربری و رمز عبور را وارد کنید.";
+
+        return;
+
+    }
+
+    const { data,error } = await supabase
+
+    .from("users")
+
+    .select("*")
+
+    .eq("username",username)
+
+    .eq("password",password)
+
+    .single();
+
+    if(error || !data){
+
+        document.getElementById("loginResult").innerHTML=
+        "نام کاربری یا رمز عبور اشتباه است.";
+
+        return;
+
+    }
+
+    localStorage.setItem("username",data.username);
+
+    localStorage.setItem("name",data.name);
+
+    localStorage.setItem("status",data.status);
+
+    document.getElementById("loginResult").innerHTML=
+    "✅ ورود موفق";
+
+    loadMessages();
+
+}
+
+
+
+// ================================
+// ارسال پیام
+// ================================
+
+document.getElementById("sendBtn")
+
+.addEventListener("click",sendMessage);
+
+async function sendMessage(){
+
+    const username=
+    localStorage.getItem("username");
+
+    if(!username){
+
+        alert("ابتدا وارد شوید.");
+
+        return;
+
+    }
+
+    const message=
+    document.getElementById("chatText").value.trim();
+
+    if(message==="") return;
+
+    const { error } = await supabase
+
+    .from("messages")
+
+    .insert([{
+
+        username:username,
+
+        message:message
+
+    }]);
+
+    if(error){
+
+        alert("خطا در ارسال پیام");
+
+        console.log(error);
+
+        return;
+
+    }
+
+    document.getElementById("chatText").value="";
+
+    loadMessages();
+
+}
+
+
+
+// ================================
+// دریافت پیام ها
+// ================================
+
+async function loadMessages(){
+
+    const { data,error } = await supabase
+
+    .from("messages")
+
+    .select("*")
+
+    .order("created_at",{ascending:true});
+
+    if(error){
+
+        console.log(error);
+
+        return;
+
+    }
+
+    const box=document.getElementById("messages");
+
+    box.innerHTML="";
+
+    data.forEach(item=>{
+
+        box.innerHTML+=`
+
+        <div class="msg">
+
+        <strong>${item.username}</strong>
+
+        <br>
+
+        ${item.message}
+
+        </div>
+
+        `;
+
+    });
+
+    box.scrollTop=box.scrollHeight;
+
+}
+
+setInterval(loadMessages,2000);
+
+loadMessages();
